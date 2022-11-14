ca4b5ce85c4fe73d30091e1a5898d29cc3f1927c
Thomas Bending <tbending@MacBook-Pro.local>
2022-11-14 11:59:17 +0000

    tidied up make_list.py

--- make_list.py
+++ make_list.py
@@ -3,12 +3,11 @@ with open ('the_list.txt',encoding='latin-1') as f_in:
 
 f_p = open('partial.txt','w')
 f_c = open('complete.txt','w')
-for i,line in enumerate(completed):
-    if "crosswords/quick" in line:
-        useful_stuff = line.split()[0]
-        num_type = useful_stuff.split("/")
-        if (len(num_type) == 3):
-            if (line.count('""""') > 100):
-                f_p.write (num_type[2]+"\n")
-            f_c.write (num_type[2]+"\n")
+for line in completed:
+    col0 = line.split()[0]
+    if "crosswords/quick" in col0:
+        num = col0.split("/")[2]
+        if (line.count('""""') > 100):
+            f_p.write (num+"\n")
+        f_c.write (num+"\n")
 
